a3464c15691024e4a36c229028be17020e845c34
AydarSS <www.sabitaidrl@mail.ru>
2024-02-24 19:26:56 +0300

    homework_1

--- src/main/java/guru/qa/country/service/CountryService.java
+++ src/main/java/guru/qa/country/service/CountryService.java
@@ -1,11 +1,13 @@
 package guru.qa.country.service;
 
+import guru.qa.country.data.CountryEntity;
 import guru.qa.country.data.CountryRepository;
 import guru.qa.country.model.Country;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,5 +25,21 @@ public class CountryService {
         .stream()
         .map(Country::fromEntity)
         .toList();
+  }
+
+  public Country saveCountry(Country country) {
+    return Country.fromEntity(
+        countryRepository.save(
+            Country.fromJson(country)));
+  }
+
+  public Country updateCountry(String id, Country countryPatch) {
+    CountryEntity modifiedCountry =  countryRepository
+        .findById(UUID.fromString(id))
+        .orElseThrow(RuntimeException::new); //not implemented Exception
+    modifiedCountry.setCountryName(countryPatch.countryName());
+    modifiedCountry.setCountryCode(countryPatch.countryCode());
+
+    return Country.fromEntity(countryRepository.save(modifiedCountry));
   }
 }
